a3a95aca4959526ef20d7644126e624269ef8ad6
Josh Siegel <jsiegel@jumptrading.com>
2022-02-17 12:25:08 +0000

    main: save this @ gusc1a-ossdev-jsl1

--- foo.go
+++ foo.go
@@ -1,5 +1,13 @@
 // https://github.com/algorand-devrel/demo-avm1.1
 
+// go get -d github.com/algorand/go-algorand-sdk/...
+// go get -u github.com/algorand/indexer/fetcher
+//
+// # github.com/algorand/go-algorand/crypto
+// ../../go/pkg/mod/github.com/algorand/go-algorand@v0.0.0-20220216190355-cbf1939eeb26/crypto/batchverifier.go:33:11: fatal error: sodium.h: No such file or directory
+//   33 | // #include "sodium.h"
+
+
 package main
 
 import (
